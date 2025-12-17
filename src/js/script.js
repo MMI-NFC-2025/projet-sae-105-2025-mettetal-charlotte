3d2b62c93811fa41a856d4d5284d81e20389f020
mettetal-charlotte <charlotte.mettetal@edu.univ-fcomte.fr>
2025-12-17 13:55:13 +0100

    amélioration carousel

--- src/js/script.js
+++ src/js/script.js
@@ -4,20 +4,29 @@ const carousel = document.querySelector(".carousel__track");
 const prevButton = document.querySelector(".carousel__button--prev");
 const nextButton = document.querySelector(".carousel__button--next");
 
-if (carousel){
-    prevButton.addEventListener("click", () =>{
-        carousel.scrollBy({
-            left: -carousel.offsetWidth,
-            behavior: "smooth",
-        });
-    });
+if (carousel) {
+    const scrollByAmount = () => {
+        const slide = document.querySelector('.carousel__slide');
+        return slide ? slide.clientWidth : carousel.offsetWidth || 260;
+    };
 
-    nextButton.addEventListener("click," () =>{
-        carousel.scrollBy({
-            left: carousel.offsetWidth,
-            behavior: "smooth",
+    if (prevButton) {
+        prevButton.addEventListener("click", () => {
+            carousel.scrollBy({
+                left: -scrollByAmount(),
+                behavior: "smooth",
+            });
         });
-    });
+    }
+
+    if (nextButton) {
+        nextButton.addEventListener("click", () => {
+            carousel.scrollBy({
+                left: scrollByAmount(),
+                behavior: "smooth",
+            });
+        });
+    }
 }
 const premiereImage = document.querySelector(".carousel__slide");
 const scrollAmount = premiereImage ? premiereImage.clientWidth : 260;

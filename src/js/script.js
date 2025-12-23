30a351ae9c75fb68e179a5216f3105fdaf500f2a
mettetal-charlotte <charlotte.mettetal@edu.univ-fcomte.fr>
2025-12-23 10:19:19 +0100

    menu

--- src/js/script.js
+++ src/js/script.js
@@ -30,3 +30,22 @@ if (carousel) {
 }
 const premiereImage = document.querySelector(".carousel__slide");
 const scrollAmount = premiereImage ? premiereImage.clientWidth : 260;
+
+/*Menu*/
+
+const toggle = document.querySelector('.menu-btn');
+const nav = document.querySelector('.menu');
+const logo = document.querySelector('.header__logo');
+const page = document.body;
+
+if (toggle && nav) {
+    toggle.addEventListener('click', () => {
+        const isOpen = toggle.getAttribute('aria-expanded') === 'true';
+        const isClosed = !isOpen;
+
+        toggle.setAttribute('aria-expanded', String(isClosed));
+        nav.hidden = isOpen;
+        if (logo) logo.classList.toggle('header__logo--extend', isClosed);
+        page.classList.toggle('u-noscroll', isClosed);
+    });
+}
